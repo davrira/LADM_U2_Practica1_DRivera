b05fac20fc6573ffe89d440e57d4514e242bb217
david <davrira>
2020-10-24 12:07:29 -0600

    comit inicial

---
+++ app/src/main/java/mx/tecnm/ladm_u2_practica1_drivera/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.tecnm.ladm_u2_practica1_drivera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
